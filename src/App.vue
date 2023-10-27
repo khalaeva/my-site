4a8174dbbfd9fac9fe2b14b9d20de7ce3f186dfc
khalaeva <halaevaanna@gmail.com>
2023-10-27 13:00:03 +0300

    .

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
+  <Header/>
   <div class="app">
     dsfdf 
   </div>
+  <Footer/>
 </template>
 
 <script setup>
-
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
 </script>
 
 <style scoped lang="scss">
 @import "@/styles/style";
 @import "@/styles/fonts";
 .app {
-  background-color: $background-color;
-  padding: 0;
-  margin: 0;
   font-family: 'MainFont';
 }
 </style>
